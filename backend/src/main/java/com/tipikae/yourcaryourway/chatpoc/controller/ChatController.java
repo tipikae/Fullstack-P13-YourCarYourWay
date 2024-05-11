b3248418cc894585fdda336ad2aa6dbb7f5bc631
Gilles Bernard <gilles.bd@free.fr>
2024-05-11 15:13:33 +0200

    Controller created

---
+++ backend/src/main/java/com/tipikae/yourcaryourway/chatpoc/controller/ChatController.java
@@ -0,0 +1,25 @@
+package com.tipikae.yourcaryourway.chatpoc.controller;
+
+import com.tipikae.yourcaryourway.chatpoc.model.InputMessage;
+import com.tipikae.yourcaryourway.chatpoc.model.OutputMessage;
+import com.tipikae.yourcaryourway.chatpoc.service.IChatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Chat controller.
+ */
+@Controller
+public class ChatController {
+
+    @Autowired
+    private IChatService chatService;
+
+    @MessageMapping("/chat")
+    @SendTo("/topic/support")
+    public OutputMessage send(InputMessage inputMessage) throws Exception{
+        return chatService.handleMessage(inputMessage);
+    }
+}
