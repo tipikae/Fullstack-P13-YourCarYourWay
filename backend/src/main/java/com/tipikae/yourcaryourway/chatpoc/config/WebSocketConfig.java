cf83f1d624b7693e8f7b0fcada8a44b3cfff6fb3
Gilles Bernard <gilles.bd@free.fr>
2024-05-11 15:12:55 +0200

    WebSocket broker configured

---
+++ backend/src/main/java/com/tipikae/yourcaryourway/chatpoc/config/WebSocketConfig.java
@@ -0,0 +1,33 @@
+package com.tipikae.yourcaryourway.chatpoc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+ * WebSocket server configuration.
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    /**
+     * Message broker configuration.
+     * @param config Message broker registry.
+     */
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    /**
+     * Stomp endpoints register.
+     * @param registry Stomp endpoints registry.
+     */
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat");
+        registry.addEndpoint("/chat").withSockJS();
+    }
+}
