a4408a5deb9653abc26441e6bf7205c18b82893d
Gilles Bernard <gilles.bd@free.fr>
2024-05-12 10:15:31 +0200

    ReactiveFormsModule imported

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ChatComponent } from './component/chat/chat.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { ChatComponent } from './component/chat/chat.component';
     ChatComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
